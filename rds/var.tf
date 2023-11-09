a6eb158bc244932ccc3be880b286c6277e630e10
Thuba1978 <victordube@yahoo.com>
2023-11-08 21:35:17 -0600

    adding vpc, rds code

---
+++ rds/var.tf
@@ -0,0 +1,14 @@
+ variable "private_subnet1" {
+    type = string
+    description = "id of subnet"
+}
+
+ variable "vpc_id" {
+    type = string
+    description = "vpc id"
+}
+
+variable "vpc_cidr" {
+    type = string
+    description = "cidr range"
+}
